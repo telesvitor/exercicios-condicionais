e36fab996fa919f3c5226a2cb495f1d5d59ca53a
vitor <vitor.teles.pereira@escola.pr.gov.br>
2025-09-11 07:53:57 -0300

    'AtvFeitas_11/09/2025'

---
+++ 5a.js
@@ -0,0 +1,16 @@
+let cor = 'azul';
+
+if  (cor != 'azul' && cor && 'vermelho' && cor != 'marrom') {
+ console.log('cor invalida')
+}
+ switch (cor) {
+    case 'vermelho':
+        console.log('cor quente')
+        break;
+    case 'azul':
+        console.log('cor fria')
+        break;
+    case 'marrom':
+        console.log('cor neutra')
+        break;
+ }
